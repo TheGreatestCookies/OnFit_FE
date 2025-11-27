9330260cce7bcfa2364723353b9d6ea639cc7062
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-11-28 01:38:07 +0900

    feat: 라우터 설정 및 npm format 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+/**
+ * Home page component
+ * @returns {JSX.Element}
+ */
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ src/router/Routes.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes as RouterRoutes } from 'react-router-dom';
+import Home from '../pages/Home';
+/**
+ * Routes component
+ * @returns {JSX.Element}
+ */
+const Routes = () => {
+  return (
+    <RouterRoutes>
+      <Route path="/" element={<Home />} />
+    </RouterRoutes>
+  );
+};
+
+export default Routes;
